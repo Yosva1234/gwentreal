78c679257b0582b954fae8e8d28b814a1baaf1c0
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2024-03-06 22:04:41 -0500

    n

---
+++ UNITYGWENT/Assets/CAMBIOS.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+   using UnityEngine.UI;
+
+   public class ModificarTexto : MonoBehaviour
+   {
+       public TMP_Text NOMBRE ;  // Variable para guardar el componente Text
+
+       void Start()
+       {
+          NOMBRE.text = "H ";
+       }
+
+       
+   }
